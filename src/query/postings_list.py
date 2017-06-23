3e4461a1c9459d871425ce815747792df84e23b5
probinso <pmoss.robinson@gmail.com>
2017-06-22 17:00:03 -0700

    start branch

--- src/query/postings_list.py
+++ src/query/postings_list.py
@@ -5,27 +5,13 @@ from collections import namedtuple
 from functools   import partial, reduce
 
 # Installed Modules
-from sortedcontainers import SortedList
+from sortedcontainers import SortedListWithKey
 
 # Local
 from utilities import SortedDefaultDict
 
 
-class _OpLoader:
-    """
-    We require all document formats to have an `doc_id` feild, for documents
-    comparison operators which are only ids values
-    __eq__
-    __lt__
-    """
-    def __eq__(self, other):
-        return self.doc_id == other.doc_id
-
-    def __lt__(self, other):
-        return self.doc_id < other.doc_id
-
-
-class Document(_OpLoader, namedtuple("Document", ["doc_id"])):
+class Document(namedtuple("Document", ["doc_id"])):
     """
     TFD is a 'Term Frequency Document' that stores a doc_id and word_count
     """
@@ -57,13 +43,14 @@ class LookupTable(InvertedIndex):
         return reduce(operator, acc)
 
 
-class PostingList(SortedList):
+class PostingList(SortedListWithKey):
     """
     Extends SortedList with 'and' operator that performs an intersection with
     another PostingList
     """
     def __init__(self, constructor, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        self._getkey = attrgetter('doc_id')
+        super().__init__(key=self._getter, *args, **kwargs)
         assert(callable(constructor))
         self._constructor = constructor
 
@@ -86,13 +73,15 @@ class PostingList(SortedList):
             if i_doc is done or j_doc is done:
                 break
 
-            if i_doc == j_doc:
+            i_key, j_key = self._getkey(i_doc), self._getkey(j_doc)
+
+            if i_key == j_key:
                 acc.append(i_doc)
                 i_doc = safe_next(it)
                 j_doc = safe_next(jt)
                 continue
 
-            if i_doc < j_doc:
+            if i_key < j_key:
                 i_doc = safe_next(it)
             else:
                 j_doc = safe_next(jt)
@@ -117,13 +106,15 @@ class PostingList(SortedList):
                 acc.extend(it)
                 break
 
-            if i_doc == j_doc:
+            i_key, j_key = self._getkey(i_doc), self._getkey(j_doc)
+
+            if i_key == j_key:
                 acc.append(i_doc)
                 i_doc = safe_next(it)
                 j_doc = safe_next(jt)
                 continue
 
-            if i_doc < j_doc:
+            if i_key < j_key:
                 acc.append(i_doc)
                 i_doc = safe_next(it)
             else:
